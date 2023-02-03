b6ec47c81ccac0d78d02321b103e2f248ec5230e
KChandrashekharaiah <Kumar.Chandrashekharaiah@smartstream-stp.com>
2023-02-03 12:44:33 +0530

    Newly added files

---
+++ Demo/com/example/Demo.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<Integer> l = (List<Integer>) null;
+		for(Integer a:l) {
+			System.out.println(a);
+		}
+
+	}
+
+}
